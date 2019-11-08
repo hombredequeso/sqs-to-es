e27310749c21c4ff091d73bfcfd9de5984237ec4
Mark Cheeseman <mcheeseman@seek.com.au>
2019-11-08 18:11:58 +1100

    Cleanup tests

--- src/test/scala/example/ElasticsearchFlowSpec.scala
+++ src/test/scala/example/ElasticsearchFlowSpec.scala
@@ -8,18 +8,19 @@ import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Keep, RunnableGraph, Sin
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.concurrent.duration._
-import scala.concurrent.{Await, Future}
+import scala.concurrent.{Await, ExecutionContext, Future}
 import org.apache.http.HttpHost
 import org.elasticsearch.client.RestClient
 import spray.json._
 import DefaultJsonProtocol._
 import akka.stream.alpakka.elasticsearch._
 
-class ElasticsearchFlowSpec
-  extends FlatSpec
-    with Matchers {
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class ElasticsearchFlowSpec extends FlatSpec with Matchers {
 
   case class Entity(id: Int, description: String)
+  case class Message[T](e: T)
 
   implicit val client: RestClient = RestClient.builder(new HttpHost("localhost", 9200)).build()
   implicit val format: JsonFormat[Entity] = jsonFormat2(Entity)
@@ -27,34 +28,29 @@ class ElasticsearchFlowSpec
   implicit val system = ActorSystem("QuickStart")
   implicit val materializer = ActorMaterializer()
 
+  val esWriterSettings = ElasticsearchWriteSettings()
+    .withBufferSize(2)
+    .withVersionType("internal")
+    .withRetryLogic(RetryAtFixedRate(maxRetries = 5, retryInterval = 1.second))
+
+
   "Simple Stream" should "be able to be run in a test" in {
 
-    val esWriterSettings = ElasticsearchWriteSettings()
-      .withBufferSize(2)
-      .withVersionType("internal")
-      .withRetryLogic(RetryAtFixedRate(maxRetries = 5, retryInterval = 1.second))
-
-    val entities = (1 to 10).map(i => Entity(i, s"entity #${i}"))
-    val source = Source(entities)
+    val entities =
+      (1 to 10).map(i => Entity(i, s"entity #${i}"))
     val esFlow: Flow[WriteMessage[Entity, NotUsed], WriteResult[Entity, NotUsed], NotUsed] =
       ElasticsearchFlow.create[Entity]("entity", "_doc", esWriterSettings)
-    val sink = Sink.ignore
 
-    // val pipeline: RunnableGraph[Future[Done]] =
-      // val r: Future[Seq[Unit]] = source
-    val r = source
+    for {
+      writeResults <- Source(entities)
         .map(e => WriteMessage.createUpsertMessage(e.id.toString, e))
         .via(esFlow)
-      .map(wr => println(s"Success: ${wr.success}; message: ${wr.message}"))
-      .runWith(sink)
-        // .runWith(Sink.seq)
-        // .toMat(sink)(Keep.right)
-        // pipeline.run()
-
-    Await.result(r, 3.seconds)
+        .wireTap(x => println(s"Result: ${x.success}"))
+        .runWith(Sink.seq)
+      assertAllMessagesProcessed = writeResults.length should ===(entities.length)
+    } yield assertAllMessagesProcessed
   }
 
-  case class Message[T](e: T)
 
   "Elasticsearch Stream" should "use passthrough to maintain original message" in {
 
@@ -63,24 +59,19 @@ class ElasticsearchFlowSpec
         .map(i => Entity(i, s"entity #${i}"))
         .map(Message(_))
 
-    val esWriterSettings = ElasticsearchWriteSettings()
-      .withBufferSize(5)
-      .withVersionType("internal")
-      .withRetryLogic(RetryAtFixedRate(maxRetries = 5, retryInterval = 1.second))
     val esFlow =
       ElasticsearchFlow.createWithPassThrough[Entity, Message[Entity]]("entity2", "_doc", esWriterSettings)
 
-    val successfulWrites: Future[Seq[(Message[Entity], WriteResult[Entity, Message[Entity]])]] =
+    for {
+      processedMessages  <-
       Source(sourceMessages)
-      .map(m => WriteMessage.createUpsertMessage(m.e.id.toString, m.e).withPassThrough(m))
-      .via(esFlow)
-      .map(wr => (wr.message.passThrough, wr))
-      .wireTap(x => println(s"Result: ${x._2.success}; message = ${x._1}"))
-      .filter(_._2.success)
-      .runWith(Sink.seq)
-
-    val result: Seq[(Message[Entity], WriteResult[Entity, Message[Entity]])] =
-      Await.result(successfulWrites, 3.seconds)
-    result.map(_._1).sortBy(x => x.e.id) should ===(sourceMessages)
+        .map(m => WriteMessage.createUpsertMessage(m.e.id.toString, m.e).withPassThrough(m))
+        .via(esFlow)
+        .map(wr => (wr.message.passThrough, wr))
+        .wireTap(x => println(s"Result: ${x._2.success}; message = ${x._1}"))
+        .filter(_._2.success)
+        .runWith(Sink.seq)
+      assertion = processedMessages .map(_._1).sortBy(x => x.e.id) should ===(sourceMessages)
+    } yield assertion
   }
 }
